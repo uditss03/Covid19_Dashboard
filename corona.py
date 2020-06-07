c6da724e66240afe7692978ced6d8c9b3053af40
udit sharma <uditss03@gmail.com>
2020-06-07 09:24:41 +0530

    csv file Location changed

--- corona.py
+++ corona.py
@@ -10,6 +10,7 @@ try:
     complete_data = pd.read_csv("https://raw.githubusercontent.com/imdevskp/covid-19-india-data/master/complete.csv")
 except:
     complete_data = pd.read_csv('complete.csv')
+#complete_data = pd.read_csv('complete.csv')
 complete_data =complete_data.drop(
   ['Total Confirmed cases (Indian National)',
   'Total Confirmed cases ( Foreign National )'],
